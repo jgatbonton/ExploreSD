fb070160e108b6cdef77ac247f005b252da8d33f
Judd Gatbonton <juddgatbonton@MacBook-Pro.local>
2017-06-29 06:25:57 -0700

    port change again

--- exploreSD/server.js
+++ exploreSD/server.js
@@ -16,10 +16,9 @@ app.get('/locations', function (req, res) {
    res.send(config);
 })
 
-
 var server = app.listen(8080, function () {
    var host = server.address().address
-   var port = server.address().port || process.env.PORT
+   var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
 })
