015fe24334a446fa22437057bbefd3d27c69f449
Judd Gatbonton <juddgatbonton@MacBook-Pro.local>
2017-06-29 05:46:48 -0700

    changed host

--- exploreSD/server.js
+++ exploreSD/server.js
@@ -10,16 +10,16 @@ app.get('/', function (req, res) {
    console.log(config.name + ' ' + config.description);
    res.send('Hello World');
 
-   
+
 })
 
 app.get('/locations', function (req, res) {
    res.send(config);
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
-   
+
    console.log("Example app listening at http://%s:%s", host, port)
 })
